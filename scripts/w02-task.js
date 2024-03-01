55a9e19684b54becfb564b050e33fba9bc94705b
ruskjm <rus23008@byui.edu>
2024-02-29 17:20:52 -0800

    week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,26 +3,29 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+const fullName = "Jason Rusk";
+const now = new Date();
+const currentYear = now.getFullYear();
+const profilePicture = "images/IMG_20230825_184555930.jpg";
 
 
 /* Step 3 - Element Variables */
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('main#home picture img');
 
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`
+yearElement.textContent = currentYear;
+imageElement.setAttribure('src', profilePicture);
 
 /* Step 5 - Array */
 
+let favoriteFoods = ["Pizza", "Ice Cream", "Hamburgers", "Cookies", "Lobster", "Pulled Pork"]
 
+foodElement.innerHTML;
 
 
 
