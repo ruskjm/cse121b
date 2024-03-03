38753e63bda97e0c89740205b80d78b9d55d373f
ruskjm <rus23008@byui.edu>
2024-03-02 16:46:11 -0800

    week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,29 +4,55 @@
 
 /* Step 2 - Variables */
 const fullName = "Jason Rusk";
-const now = new Date();
-const currentYear = now.getFullYear();
+const currentYear = new Date().getFullYear();
 const profilePicture = "images/IMG_20230825_184555930.jpg";
 
-
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('food');
-const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('main#home picture img');
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("main#home picture img");
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`
 yearElement.textContent = currentYear;
-imageElement.setAttribure('src', profilePicture);
+imageElement.setAttribute("src", profilePicture);
 
 /* Step 5 - Array */
 
-let favoriteFoods = ["Pizza", "Ice Cream", "Hamburgers", "Cookies", "Lobster", "Pulled Pork"]
+// Set the array value
+let favoriteFoods = ["Pizza", "Ice Cream", "Hamburgers", "Cookies", "Lobster", "Pulled Pork"];
 
-foodElement.innerHTML;
+// Add to the array to foodHtml variable seperated with comma/space
+let foodHtml = favoriteFoods.join(", "); 
 
+// Display the array
+document.getElementById("food").innerHTML = foodHtml;
 
+// Append new food
+favoriteFoods.push("Tacos"); 
 
+// Add to the foodHtml value with commas
+foodHtml = favoriteFoods.join(", ");
+
+// Display the array
+document.getElementById("food").innerHTML += "<br>" + foodHtml;
+
+// Remove first element from the array
+favoriteFoods.shift();
+
+// Add to the array to foodHtml variable seperated with comma/space
+foodHtml = favoriteFoods.join(", ");
+
+// Display the array
+document.getElementById("food").innerHTML += "<br>" + foodHtml;
+
+// Remove last element from the array
+favoriteFoods.pop();
+
+// Add to the array to foodHtml variable seperated with comma/space
+foodHtml = favoriteFoods.join(", ");
+
+// Display the array
+document.getElementById("food").innerHTML += "<br>" + foodHtml;
 
